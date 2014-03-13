54c697a6eab1c889eccf1c7ba8822445490a7ae6
Matt Dumford <mdumford99@gmail.com>
2014-03-13 18:44:17 -0500

    started mysqlconnector class

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -2,6 +2,20 @@ import java.sql.*;
 
 public class EmployeeManager{
 	public static void main(String args[]){
-		System.out.println("Hello, World");
+
+		if(args.length < 4)
+			System.exit(1);
+
+		String db = args[1];
+		String user = args[2];
+		String pass = args[3];
+
+		MySQLConnector conn;
+		try{
+			conn = new MySQLConnector("localhost", 3306, db, user, pass);
+			conn.createTables();
+		} catch(Exception e){e.printStackTrace();}
+
+		
 	}
 }
---
+++ src/MySQLConnector.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class MySQLConnector{
+
+	private Connection connection;
+
+	public MySQLConnector(String host, int port, String database, String user, String password) throws Exception{
+		Class.forName("com.mysql.jdbc.Driver");
+		connection = DriverManager.getConnection(
+				"jdbc:mysql://" + host + ":" + port + "/" + database, user, password);
+	}
+
+	public void close() throws SQLException{
+		connection.close();
+	}
+
+	public void createTables() throws SQLException{
+		Statement st = connection.createStatement();
+		String createEmployee = "CREATE TABLE employee " +
+								"(eid INTEGER not NULL, " +
+								" name VARCHAR(20), " + 
+								" salary INTEGER, " +
+								" PRIMARY KEY (eid))";
+		String createWorksfor = "CREATE TABLE worksfor " + 
+								"(eid INTEGER, " + 
+								" mid INTEGER)";
+
+		st.executeUpdate(createEmployee);
+		st.executeUpdate(createWorksfor);
+	}
+
+	public void dropTables() throws SQLException{
+		Statement st = connection.createStatement();
+		st.executeUpdate("DROP TABLE employee");
+		st.executeUpdate("DROP TABLE worksfor");
+	}
+}
